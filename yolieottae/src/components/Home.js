1c933b5ca2c3a6191b428402439c570b7ad497db
Lim_JiSeon <dpf226@naver.com>
2022-07-23 21:23:10 +0900

    Download fontawesome

--- yolieottae/src/components/Home.js
+++ yolieottae/src/components/Home.js
@@ -1,12 +1,20 @@
 import { examWrite } from "../utils/firebaseConfig";
+import subImage1 from "../img/food-img1.png";
 import Header from "./Header";
 
 function Home() {
   return (
     <div>
       <Header></Header>
-      <section>
-        
+      <section class="sub-coment">
+        <div class="mini-coment">
+          음식엔 다양한 사연들이 있다.<br/>
+          음식이 만들어지는 과정까지 그 사람이<br/> 
+          겪은 과정들이 있어야 나오는 음식.
+        </div>
+        <div class="img-div">
+          <img class="sub-img" alt="sub-img1" src={subImage1} />
+        </div>
       </section>
       <section>
         
