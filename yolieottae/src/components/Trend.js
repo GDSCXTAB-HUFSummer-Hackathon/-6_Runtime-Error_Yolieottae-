d730d0f2a0aefb84a5ef4d8799e32c69a47b0250
Lim_JiSeon <dpf226@naver.com>
2022-07-24 02:42:12 +0900

    UPDATE Trend

--- yolieottae/src/components/Trend.js
+++ yolieottae/src/components/Trend.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import trendImage1 from "../img/trend-img.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCarrot } from "@fortawesome/free-solid-svg-icons";
 import { faUtensils } from "@fortawesome/free-solid-svg-icons"; 
@@ -9,33 +10,35 @@ import { faKitchenSet } from "@fortawesome/free-solid-svg-icons";
 function Trend() {
   return (
     <div class="wrap">
-      <Header></Header>
+        <Header></Header>
+        <section class="trend-bar">
+            <img class="trend-bar-img" src={trendImage1} />
+        </section>
+        <section class="keyword-trend">
+            <button class="icon-food">
+                <FontAwesomeIcon icon={faCarrot} size="5x" />
+            </button>
+            <button class="icon-mealkit" href="">
+                <FontAwesomeIcon icon={faKitchenSet} size="5x" />
+            </button>
+            <button class="icon-theme" href="">
+                <FontAwesomeIcon icon={faUtensils} size="5x" />
+            </button>
+            <button class="icon-health" href="">
+                <FontAwesomeIcon icon={faHandHoldingMedical} size="5x" />
+            </button>
+        </section>
+        <section>
 
-      <section class="keyword-trend">
-        <button class="icon-food">
-            <FontAwesomeIcon icon={faCarrot} size="5x" />
-        </button>
-        <button class="icon-mealkit" href="">
-            <FontAwesomeIcon icon={faKitchenSet} size="5x" />
-        </button>
-        <button class="icon-theme" href="">
-            <FontAwesomeIcon icon={faUtensils} size="5x" />
-        </button>
-        <button class="icon-health" href="">
-            <FontAwesomeIcon icon={faHandHoldingMedical} size="5x" />
-        </button>
-      </section>
-      <section>
-
-      </section>
-      
-      <footer>
-        <div class="footer-div">
-          <p class="footer-content">
-            Runtime Error &copy; 요리어때?
-          </p>
-        </div>
-      </footer>
+        </section>
+        
+        <footer>
+            <div class="footer-div">
+                <p class="footer-content">
+                Runtime Error &copy; 요리어때?
+                </p>
+            </div>
+        </footer>
     </div>
   );
 }
