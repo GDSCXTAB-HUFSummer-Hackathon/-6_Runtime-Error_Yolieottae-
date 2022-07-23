138440e5b116f7ee2a1118cfebd009d70690815a
yseojin1@gmail.com <yseojin1@gmail.com>
2022-07-24 08:58:52 +0900

    Update index.css

--- yolieottae/src/components/Baby.js
+++ yolieottae/src/components/Baby.js
@@ -11,19 +11,19 @@ function Baby(){
             <h2>단호박 닭가슴살 이유식</h2>
             <h3>8개월차 중기 아기 먹일 이유식</h3>
             <ul class="soup">
-                <li class="ingredient">닭가슴살</li>
-                <li class="ingredient">단호박</li>
-                <li class="ingredient">불린쌀</li>
-                <li class="ingredient">다시마</li>
+                <li class="ingredient">#닭가슴살</li>
+                <li class="ingredient">#단호박</li>
+                <li class="ingredient">#불린쌀</li>
+                <li class="ingredient">#다시마</li>
             </ul>
             <img width="400" src={baby2} alt="baby2"></img>
             <h2>난자완스 이유식</h2>
             <h3>13개월차 완료기 아기 먹일 이유식</h3>
             <ul class="soup">
-                <li class="ingredient">다시마</li>
-                <li class="ingredient">양파</li>
-                <li class="ingredient">버섯</li>
-                <li class="ingredient">호박</li>
+                <li class="ingredient">#다시마</li>
+                <li class="ingredient">#양파</li>
+                <li class="ingredient">#버섯</li>
+                <li class="ingredient">#호박</li>
             </ul>
         </div>
     )
--- yolieottae/src/components/Pet.js
+++ yolieottae/src/components/Pet.js
@@ -11,22 +11,22 @@ function Pet(){
             <h2>강아지 파르페</h2>
             <h3>강아지를 위한 화식 파르페</h3>
             <ul class="dog">
-                <li class="ingredient">브로콜리</li>
-                <li class="ingredient">당근</li>
-                <li class="ingredient">두부</li>
-                <li class="ingredient">단호박</li>
-                <li class="ingredient">바나나</li>
+                <li class="ingredient">#브로콜리</li>
+                <li class="ingredient">#당근</li>
+                <li class="ingredient">#두부</li>
+                <li class="ingredient">#단호박</li>
+                <li class="ingredient">#바나나</li>
             </ul>
 
             <img width="400" src={pet2} alt="pet2"></img>
             <h2>강아지 황태닭가슴살 스틱</h2>
             <h3>닭가슴살로 만든 여러가지 강아지 간식</h3>
             <ul class="dog">
-                <li class="ingredient">닭가슴살</li>
-                <li class="ingredient">베지믹스</li>
-                <li class="ingredient">식초</li>
-                <li class="ingredient">황태파우더</li>
-                <li class="ingredient">코코넛슬라이스</li>
+                <li class="ingredient">#닭가슴살</li>
+                <li class="ingredient">#베지믹스</li>
+                <li class="ingredient">#식초</li>
+                <li class="ingredient">#황태파우더</li>
+                <li class="ingredient">#코코넛슬라이스</li>
             </ul>
         </div>
     )
